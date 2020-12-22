cedb32909d92abb547e36fd20e6ed3e00b7830d2
Ibrahim <alzuabi.ibr@gmail.com>
2020-12-22 12:36:59 +0200

    Add watcher

--- Service1.cs
+++ Service1.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsServiceTest1.Service;
 
 namespace WindowsServiceMoveFiles
 {
     public partial class Service1 : ServiceBase
     {
+        private Watcher fWatcher = Watcher.Instance;
+       
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +23,13 @@ namespace WindowsServiceMoveFiles
 
         protected override void OnStart(string[] args)
         {
+            fWatcher.WriteToFile("Service is started at " + DateTime.Now);
+            fWatcher.GetWatcherForDirectory(@"C:\Users\ASUS\MultiSys\source");
         }
 
         protected override void OnStop()
         {
+            fWatcher.WriteToFile("Service is stopped at " + DateTime.Now);
         }
     }
 }
--- Watcher.cs
+++ Watcher.cs
@@ -111,6 +111,31 @@ namespace WindowsServiceTest1.Service
             return new Watcher(watcher);
         }
 
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+
         public void Dispose()
         {
 
